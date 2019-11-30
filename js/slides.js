ccd3a9d2ab5f272bd2f36eda5c90fc6bcc4bc013
Qiang-1028 <54606089+Qiang-1028@users.noreply.github.com>
2019-11-29 19:20:39 -0600

    1

--- js/slides.js
+++ js/slides.js
@@ -3,7 +3,8 @@ let slideIndex = 0;
 const showSlides = () => {
 
   const slides = document.getElementsByClassName("slides");
-  
+  const queue = document.getElementsByClassName("slides");
+
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -14,5 +15,9 @@ const showSlides = () => {
     slideIndex = 1;
   }
 
+  slides[slideIndex - 1].style.display = "block";
+  queue[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 1000);
 
+};
 showSlides();
