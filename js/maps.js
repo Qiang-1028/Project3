96ed6607b7c3d4ff328e5703608b9a350150eb3c
Qiang-1028 <54606089+Qiang-1028@users.noreply.github.com>
2019-12-04 19:41:19 -0600

    formatting

--- js/maps.js
+++ js/maps.js
@@ -1,13 +1,50 @@
-<script>
-    function initMap() {
-      var home = {lat: 41.832042, lng:-87.612864};
-      var school = {lat:41.835134, lng:-87.626981};
-      var gym = {lat:41.832133, lng:-87.615088};
-      var map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center: home});
-      var marker = new google.maps.Marker({position: home, map: map});
-      var marker2 = new google.maps.Marker({position:school, map: map});
-      var marker3 = new google.maps.Marker({position:gym, map: map});}
-  </script>
-  <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDvQrwL5WpdEQ-wKv7CKDbWER2UwYE-b-k&callback=initMap">
-  </script>
+   function initMap() {
+      var home = {
+        lat: 41.832042, 
+        lng:-87.612864
+      };
+      var school = {
+        lat:41.835134,
+        lng:-87.626981
+      };
+      var gym = {
+        lat:41.832133, 
+        lng:-87.615088
+      };
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: home
+      });
+      var marker = new google.maps.Marker({
+        position: home,
+        map: map
+      });
+      var marker2 = new google.maps.Marker({
+        position:school, 
+        map: map
+      });
+      var marker3 = new google.maps.Marker({
+        position:gym, 
+        map: map
+      });
+      var infoWindow = new google.maps.InfoWindow({
+        content: 'My home'
+      });
+      marker.addListener('click', function () {
+       infoWindow.open(map, marker);
+      })
+      var infoWindow2 = new google.maps.InfoWindow({
+        content: 'My School'
+      });
+      marker2.addListener('click', function () {
+        infoWindow2.open(map, marker2);
+      })
+      var infoWindow3 = new google.maps.InfoWindow({
+        content: 'My Gym'
+      });
+      marker3.addListener('click', function () {
+       infoWindow3.open(map, marker3);
+      })
+      var transitLayer = new google.maps.TransitLayer();
+      }
+ 
